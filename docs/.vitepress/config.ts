38e2869b677adc8d7a668f4b5e03fdacaf1caf88
xrmtech-isk <119593737+xrmtech-isk@users.noreply.github.com>
2026-04-29 18:29:14 +0300

    Update config.ts

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -11,7 +11,17 @@ import { defineConfig } from 'vitepress';
 const links: Array<{url: string, lastmod?: number}> = []
 
 const home_sidebar = [
+  { 
+    text: 'Документация Director',
+    collapsed: true,
+    items: [
+    {text: 'Технические характеристики', link: 'XRM/datasheet.html' },
+    {text: 'Руководство админстратора', base: '/XRM/,
+      items: [{ text: 'Технические характеристики', link: 'datasheet.html' },
+             ] 
+  },
   {
+    
     text: 'Документация Director',
     collapsed: false,
     items: [
